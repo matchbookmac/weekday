6baf5817b986688354a07e95d5743054faede888
Ian MacDonald & Toby Alden <student@epicodus.com>
2015-04-21 10:01:33 -0700

    First spec failed correctly

--- spec/weekday_spec.rb
+++ spec/weekday_spec.rb
@@ -3,6 +3,6 @@ require('weekday')
 
 describe('Time#weekday') do
   it("provides the day of the week given a day in a year") do
-    expect(Time.new()).to(eq("Tuesday"))
+    expect(Time.new().weekday()).to(eq("Tuesday"))
   end
 end
