853620e85184c5aaedd4666b3bb6e57b450614e5
Ian MacDonald & Toby Alden <student@epicodus.com>
2015-04-21 10:00:06 -0700

    First spec failed

---
+++ README.md
@@ -0,0 +1,1 @@
+When the user provides a day of the year, the program will tell the user what day of the week it was.
---
+++ spec/weekday_spec.rb
@@ -0,0 +1,8 @@
+require('rspec')
+require('weekday')
+
+describe('Time#weekday') do
+  it("provides the day of the week given a day in a year") do
+    expect(Time.new()).to(eq("Tuesday"))
+  end
+end
